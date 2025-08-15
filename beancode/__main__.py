dd3be9eefdbc69488bb40a9be06b23aa0ef512ec
ezntek <eason@ezntek.com>
2025-08-15 19:22:07 +0800

    feat: add no run feature

--- beancode/__main__.py
+++ beancode/__main__.py
@@ -11,12 +11,18 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("file", type=str)
     parser.add_argument(
-        "--debug", action="store_true", help="show debugging information"
+        "-d", "--debug", action="store_true", help="show debugging information"
+    )
+    parser.add_argument(
+        "--no-run", action="store_true", help="only print the program's AST"
     )
     parser.add_argument(
         "-v", "--version", action="version", version=f"beancode version {__version__}"
     )
     args = parser.parse_args()
+
+    if args.no_run:
+        args.debug = True
 
     if not os.path.exists(args.file):
         error(f"file {args.file} does not exist!")
@@ -53,7 +59,10 @@ def main():
             print(stmt)
             print()
         print("\033[0m\033[1m----- END OF AST -----\033[0m", file=sys.stderr)
-    
+
+    if args.no_run:
+        return
+
     try:
         i = Interpreter(program.stmts)
         i.toplevel = True
@@ -61,7 +70,6 @@ def main():
     except BCError as err:
         err.print(args.file, file_content)
         exit(1)
-
 
 if __name__ == "__main__":
     main()
