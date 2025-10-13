0342b571e5c2a610620f63d44bd60a6c9a1ebdb7
ezntek <eason@ezntek.com>
2025-10-13 21:23:10 +0800

    progress(help): more messages

--- beancode/bean_help.py
+++ beancode/bean_help.py
@@ -1,8 +1,6 @@
 from io import StringIO
 from typing import Callable
 
-from .libroutine_defs import *
-
 # multiline string literals are cringe and kinda unreadable.
 # We use StringIOs here!
 
@@ -12,16 +10,32 @@ def _help() -> str:
     res = StringIO()
     print("Show this help message, and a list of available help pages.\n", file=res)
     print("Available help entries include:", file=res)
-    for (key, val) in HELP_ENTRIES.items():
+    for key, val in HELP_ENTRIES.items():
         print(f"  - \033[32m{key}\033[0m: {val[0]}", file=res)
-    print("\nType help(\"your help entry\") to get more information.")
+    print('\nType help("your help entry") to get more information.', file=res)
     return res.getvalue()
 
 
 def _libroutines() -> str:
     res = StringIO()
 
-    print()
+    print(
+        "Library routines are built-in features of Pseudocode that simplify tasks for you.",
+        file=res,
+    )
+    print(
+        """
+There are many of them; some are marked with \033[2m[beancode extension]\033[0m, which means
+that it is not an \"official\" library routine specified by Cambridge, but rather
+a custom one that beancode includes to further simplify programming.
+    """,
+        file=res,
+    )
+
+    print("Here are the ones that Beancode supports:", file=res)
+    for key, val in LIBROUTINE_ENTRIES.items():
+        print(f"  - \033[32m{key}\033[0m: {val[0]}", file=res)
+    print('\nType help("your library routine") to get more information.', file=res)
 
     return res.getvalue()
 
@@ -29,15 +43,25 @@ def _libroutines() -> str:
 def _ucase() -> str:
     res = StringIO()
 
-    # TODO: write help
+    res.write(LIBROUTINE_ENTRIES["ucase"][0] + '\n')
+    res.write("Arguments: (STRING), Returns: STRING\n\n")
+    res.write("""Examples:
+  UCASE("hello") \033[2m// returns "HELLO"\033[0m
+  UCASE("Srinivasa Ramanujan") \033[2m// returns "SRINIVASA RAMANUJAN"\033[0m
+    """)
 
     return res.getvalue()
 
 
 def _lcase() -> str:
     res = StringIO()
-
-    # TODO: write help
+    
+    res.write(LIBROUTINE_ENTRIES["lcase"][0] + '\n')
+    res.write("Arguments: (STRING), Returns: STRING\n\n")
+    res.write("""Examples:
+  LCASE("GOODBYE") \033[2m// returns "goodbye"\033[0m
+  LCASE("Mista Zozin") \033[2m// returns "mista zozin"\033[0m
+    """)
 
     return res.getvalue()
 
@@ -45,7 +69,12 @@ def _lcase() -> str:
 def _div() -> str:
     res = StringIO()
 
-    # TODO: write help
+    res.write(LIBROUTINE_ENTRIES["div"][0] + '\n')
+    res.write("Arguments: (REAL or INTEGER), Returns: REAL or INTEGER\n\n")
+    res.write("""Examples:
+  DIV(5, 2) \033[2m// returns 2\033[0m
+  DIV(12.5, 2) \033[2m// returns 6\033[0m
+    """)
 
     return res.getvalue()
 
@@ -53,7 +82,12 @@ def _div() -> str:
 def _mod() -> str:
     res = StringIO()
 
-    # TODO: write help
+    res.write(LIBROUTINE_ENTRIES["mod"][0] + '\n')
+    res.write("Arguments: (REAL or INTEGER), Returns: REAL or INTEGER\n\n")
+    res.write("""Examples:
+  MOD(5, 2) \033[2m// returns 1\033[0m
+  MOD(7.3, 4.1) \033[2m// returns 3.2\033[0m
+    """)
 
     return res.getvalue()
 
@@ -62,22 +96,19 @@ def _substring() -> str:
     res = StringIO()
 
     # TODO: write help
+    res.write(f"""{LIBROUTINE_ENTRIES["substring"][0]} 
+SUBSTRING works on the base string, where you want to begin taking characters
+and how many characters to take in total.\n
+\033[1mNote: IGCSE Pseudocode strings start at 1.\n\033[0m
+""")
+    res.write("Arguments: (STRING, INTEGER, INTEGER), Returns: STRING\n")
+    res.write("""Example:
+  DECLARE Original, First, Second: STRING
+  Original <- "Fish and Chips"
 
-    return res.getvalue()
-
-
-def _round() -> str:
-    res = StringIO()
-
-    # TODO: write help
-
-    return res.getvalue()
-
-
-def _sqrt() -> str:
-    res = StringIO()
-
-    # TODO: write help
+  First <- SUBSTRING(Original, 1, 4) \033[2m// stores "Fish" into First\033[0m
+  Second <- SUBSTRING(Original, 10, 5) \033[2m// stores "Chips" into Second\033[0m
+    """)
 
     return res.getvalue()
 
@@ -85,7 +116,47 @@ def _sqrt() -> str:
 def _length() -> str:
     res = StringIO()
 
+    res.write(LIBROUTINE_ENTRIES["length"][0] + '\n')
+    res.write("Arguments: (STRING), Returns: INTEGER\n")
+    res.write("""Examples:
+  LENGTH("Andrew Kelley") \033[2m// returns 13\033[0m
+  LENGTH("Drew DeVault") \033[2m// returns 12\033[0m
+    """)
+
+    return res.getvalue()
+
+def _round() -> str:
+    res = StringIO()
+
+    res.write(LIBROUTINE_ENTRIES["round"][0] + '\n')
+    res.write("Arguments: (REAL or INTEGER), Returns: REAL or INTEGER\n")
+    res.write("""Examples:
+  ROUND(3.1415926, 2) \033[2m// returns 3.14\033[0m
+  ROUND(5.2613, 0) \033[2m// returns 5 as an integer\033[0m
+    """)
+
+    return res.getvalue()
+
+EXTENSION_TXT = """\033[31mThis is a library routine that is part of the beancode extensions.\033[0m
+This means that it is not found in standard IGCSE Pseudocode, but is specific
+to beancode. If you use this function on an exam, the examiner will know what
+you mean, but keep in mind that it is not in the IGCSE Pseudocode standard!
+    \n"""
+
+
+def _random() -> str:
+    res = StringIO()
+
     # TODO: write help
+
+    return res.getvalue()
+
+def _sqrt() -> str:
+    res = StringIO()
+
+    res.write(LIBROUTINE_ENTRIES["sqrt"][0] + '\n')
+    res.write(EXTENSION_TXT)
+    res.write("Arguments: (REAL or INTEGER), Returns: REAL\n")
 
     return res.getvalue()
 
@@ -93,7 +164,9 @@ def _length() -> str:
 def _sin() -> str:
     res = StringIO()
 
-    # TODO: write help
+    res.write(LIBROUTINE_ENTRIES["sin"][0] + '\n')
+    res.write(EXTENSION_TXT)
+    res.write("Arguments: (REAL), Returns: REAL\n")
 
     return res.getvalue()
 
@@ -101,7 +174,9 @@ def _sin() -> str:
 def _cos() -> str:
     res = StringIO()
 
-    # TODO: write help
+    res.write(LIBROUTINE_ENTRIES["cos"][0] + '\n')
+    res.write(EXTENSION_TXT)
+    res.write("Arguments: (REAL), Returns: REAL\n")
 
     return res.getvalue()
 
@@ -109,7 +184,9 @@ def _cos() -> str:
 def _tan() -> str:
     res = StringIO()
 
-    # TODO: write help
+    res.write(LIBROUTINE_ENTRIES["tan"][0] + '\n')
+    res.write(EXTENSION_TXT)
+    res.write("Arguments: (REAL), Returns: REAL\n")
 
     return res.getvalue()
 
@@ -117,15 +194,10 @@ def _tan() -> str:
 def _getchar() -> str:
     res = StringIO()
 
-    # TODO: write help
+    res.write(LIBROUTINE_ENTRIES["getchar"][0] + '\n')
+    res.write(EXTENSION_TXT)
+    res.write("Arguments: (), Returns: CHAR\n")
 
-    return res.getvalue()
-
-
-def _random() -> str:
-    res = StringIO()
-
-    # TODO: write help
 
     return res.getvalue()
 
@@ -149,7 +221,7 @@ def _exit() -> str:
 def _sleep() -> str:
     res = StringIO()
 
-# TODO: write help
+    # TODO: write help
 
     return res.getvalue()
 
@@ -162,13 +234,16 @@ def _flush() -> str:
     return res.getvalue()
 
 
-_bcext = "\033[2m[beancode extension] \033[0m"
+_bcext = "\033[2m[beancode extension]\033[0m"
 
 LIBROUTINE_ENTRIES = {
     "ucase": ("Get the uppercase value of a string.", _ucase),
     "lcase": ("Get the lowercase value of a string.", _lcase),
     "div": ("Floor-divide a numeric value by a numeric value.", _div),
-    "mod": ("Find the remainder of a numeric value when divided by a numeric value.", _mod),
+    "mod": (
+        "Find the remainder of a numeric value when divided by a numeric value.",
+        _mod,
+    ),
     "substring": ("Extract a sub-string from a string.", _substring),
     "round": ("Round a value to a certain number of decimal places.", _round),
     "length": ("Find the length of a string.", _length),
@@ -187,12 +262,18 @@ LIBROUTINE_ENTRIES = {
 HELP_ENTRIES: dict[str, HelpEntry] = {
     "help": ("Show some information regarding the help library routine.", _help),
     "library routines": ("Information regarding library routines", _libroutines),
+    "libroutines": ("Information regarding library routines", _libroutines),
+    
     # library routines
 }
 HELP_ENTRIES.update(LIBROUTINE_ENTRIES)
+
 
 def bean_help(query: str) -> str | None:
     entry = HELP_ENTRIES.get(query.lower())
     if entry is None:
         return None
-    return f"\033[32;1m=== beancode help for \033[0m{query}\033[32;1m ===\033[0m\n" + entry[1]() 
+    return (
+        f"\033[32;1m=== beancode help for \033[0m{query}\033[32;1m ===\033[0m\n"
+        + entry[1]()
+    )
