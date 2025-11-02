b01222fc9a08ac5af9aaab1a5fb7abfc1dd414f3
ezntek <eason@ezntek.com>
2025-11-02 18:11:15 +0800

    feat(runner): open a tkinter file dialog for run_file

--- beancode/runner.py
+++ beancode/runner.py
@@ -1,12 +1,38 @@
+from beancode.error import warn, info
 
-def run(filename: str):
+def _get_file_path_with_dialog() -> str | None:
+    try:
+        # inspired by https://www.pythontutorial.net/tkinter/tkinter-open-file-dialog/
+        from tkinter import filedialog as fd
+    except ImportError:
+        warn("could not import tkinter to show a file picker!")
+        return None
+   
+    filetypes = (
+        ('Pseudocode/beancode scripts', '*.bean'),
+        ('Pseudocode scripts', '*.pseudo'),
+        ('Pseudocode scripts', '*.psc'),
+        ('Pseudocode scripts', '*.pseudocode'),
+        ('All files', '*.*'),
+    )
+    return fd.askopenfilename(title='Select file to run', initialdir='.', filetypes=filetypes)
+
+def run_file(filename: str | None = None):
+    if not filename:
+        info("opening tkinter dialog")
+        real_path = _get_file_path_with_dialog()
+        if real_path is None:
+            return
+    else:
+        real_path = filename
+
     file_content = str()
-    with open(filename, "r") as f:
+    with open(real_path, "r") as f:
         file_content = f.read()
     execute(file_content)
 
 
-def execute(src: str, filename="(execute)") -> "Interpreter": # type: ignore
+def execute(src: str, filename="(execute)", save_interpreter=False) -> "Interpreter | None": # type: ignore
     from .error import BCError
     from .lexer import Lexer
     from .parser import Parser
@@ -35,5 +61,6 @@ def execute(src: str, filename="(execute)") -> "Interpreter": # type: ignore
     except BCError as err:
         err.print(filename, src)
         exit(1)
-
-    return i
+    
+    if save_interpreter:
+        return i
