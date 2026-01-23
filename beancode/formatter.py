5cdc6e5618bf344c72e5435548a84b18b3d701f7
ezntek <eason@ezntek.com>
2026-01-23 10:09:41 +0800

    fix(formatter): add quotes for string file IDs

--- beancode/formatter.py
+++ beancode/formatter.py
@@ -419,7 +419,7 @@ class Formatter:
             self.write(" ")
             self.visit_expr(file_id)
         else:
-            self.write(f" {file_id}")
+            self.write(f" \"{file_id}\"")
 
     def visit_openfile_stmt(self, stmt: OpenfileStatement):
         self.write("OPENFILE")
